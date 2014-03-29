ac5e5e5effda603c44f9a50b25b859c18bc37770
Marcel Akerboom <marcello@dimarcello.nl>
2014-03-29 20:58:28 +0100

    Fix typo in CreateUsages.

--- db/migrate/20140329195010_create_usages.rb
+++ db/migrate/20140329195010_create_usages.rb
@@ -1,8 +1,9 @@
 class CreateUsages < ActiveRecord::Migration
   def change
     create_table :usages do |t|
-      t.reference :activity, index: true
-      t.reference :activity_variant, index: true
+      t.references :activity, index: true
+      t.references :activity_variant, index: true
+
       t.decimal :quantity
       t.string :unit
       t.text :comment
