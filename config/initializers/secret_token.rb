a909d707b6288bee952a1d43b2f37c8bf2279727
Marcel Akerboom <marcello@dimarcello.nl>
2014-03-29 18:02:05 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CleanWeb::Application.config.secret_key_base = '596bdc7b00d719e4dc4eae6d7922a4076919e0ba7131d576f1fec2b6113bb88bfd235610efa77165f47334d128d48306c646779a4a9a5fb57a413dce1517cacb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CleanWeb::Application.config.session_store :cookie_store, key: '_clean-web_session'
