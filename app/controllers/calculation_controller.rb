6d4c9f6e0aa4dad1c8a280a4910e1d4e79bbe7a7
Ahmy Yulrizka <yulrizka@gmail.com>
2014-03-30 04:26:40 +0200

    Add calculation method

--- app/controllers/calculation_controller.rb
+++ app/controllers/calculation_controller.rb
@@ -6,7 +6,18 @@ class CalculationController < ApplicationController
   end
 
   def create
+    @activities = Activity.all.includes(:variants).load
+    @conversions = Conversion.all.load
 
+    activity_variant_id = params[:calculation][:activity_variant_id]
+    conversion_id = params[:calculation][:conversion_id]
+
+    @activity_variant = ActivityVariant.find(activity_variant_id)
+    @conversion = Conversion.find(conversion_id)
+
+    @value = @conversion.convert(@activity_variant.co2)
+
+    render :new
   end
 
 end
