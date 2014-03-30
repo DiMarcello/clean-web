f5f731dc3094ea7441a56f2dae117537fe232c94
Ahmy Yulrizka <yulrizka@gmail.com>
2014-03-30 04:46:35 +0200

    wip with error

--- app/controllers/calculation_controller.rb
+++ app/controllers/calculation_controller.rb
@@ -3,11 +3,13 @@ class CalculationController < ApplicationController
   def new
     @activities = Activity.all.includes(:variants).load
     @conversions = Conversion.all.load
+    @calculation = Calculation.new
   end
 
   def create
     @activities = Activity.all.includes(:variants).load
     @conversions = Conversion.all.load
+    @calculation = Calculation.new(params[:calculation])
 
     activity_variant_id = params[:calculation][:activity_variant_id]
     conversion_id = params[:calculation][:conversion_id]
---
+++ app/models/calculation.rb
@@ -0,0 +1,4 @@
+class Calculation
+  include ActiveModel::Model
+  attr_accessor :activity_variant_id, :conversion_id
+end
