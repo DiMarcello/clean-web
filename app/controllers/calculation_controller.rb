9e6f8b4108f16e19e91ab81356bf9e661e934685
Marcel Akerboom <marcello@dimarcello.nl>
2014-03-30 03:30:12 +0200

    output select

--- app/controllers/calculation_controller.rb
+++ app/controllers/calculation_controller.rb
@@ -2,7 +2,7 @@ class CalculationController < ApplicationController
 
   def new
     @activities = Activity.all.includes(:variants).load
-    @conversions = Conversion.all
+    @conversions = Conversion.all.load
   end
 
   def create
