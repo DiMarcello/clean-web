c3f0d71b6b42472d522e946eb8196fbf72e15550
Marcel Akerboom <marcello@dimarcello.nl>
2014-03-30 03:39:18 +0200

    Added conversion method.

--- app/models/conversion.rb
+++ app/models/conversion.rb
@@ -1,6 +1,11 @@
 class Conversion < ActiveRecord::Base
 
   validates :co2, presence: true
-  validates :co2, presence: true
+  validates :name, presence: true
+  validates :unit, presence: true
+
+  def consumption
+    Unit(unit)
+  end
 
 end
